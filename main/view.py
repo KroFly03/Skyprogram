50bfc80488253de9036c2f906feb62422c73c1a4
KroFly03 <krofly03@mail.ru>
2022-11-01 15:48:38 +0300

    Add api routes

--- main/view.py
+++ main/view.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
 from utils import *
 
 main_blueprint = Blueprint('main_blueprint', __name__, template_folder='templates')
--- run.py
+++ run.py
@@ -1,9 +1,22 @@
-from flask import Flask, send_from_directory
+from flask import Flask, jsonify
 from main.view import main_blueprint
+from utils import *
 
 
 app = Flask(__name__)
 app.register_blueprint(main_blueprint)
+
+
+@app.route('/api/posts')
+def show_posts():
+    posts = load_data(POST_PATH)
+    return jsonify(posts)
+
+
+@app.route('/api/posts/<int:post_id>')
+def show_post(post_id):
+    post = get_post_by_pk(post_id)
+    return jsonify(post)
 
 
 if __name__ == '__main__':
